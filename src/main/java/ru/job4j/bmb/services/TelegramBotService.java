bd66337fa14d4195a58b9e38cbae504e73c218fb
Ivgenium <genrymorgon@mail.ru>
2025-02-28 20:30:05 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,4 +1,10 @@
 package ru.job4j.bmb.services;
 
+import org.springframework.stereotype.Service;
+import ru.job4j.bmb.content.Content;
+
+@Service
 public class TelegramBotService {
+    public void receive(Content content) {
+    }
 }
