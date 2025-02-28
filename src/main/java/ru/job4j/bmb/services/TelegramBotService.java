14d6e27daced508c0d8ada39be81c84705f4115c
Ivgenium <genrymorgon@mail.ru>
2025-02-28 22:08:00 +0300

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,10 +1,22 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
 @Service
 public class TelegramBotService {
     public void receive(Content content) {
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
     }
 }
