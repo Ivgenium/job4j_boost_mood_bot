dbaf655cb5cbc09aa764eb61ca8282008ddd3fec
Ivgenium <genrymorgon@mail.ru>
2025-03-02 23:22:12 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/services/RecommendationEngine.java
@@ -5,8 +5,26 @@ import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
+
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class RecommendationEngine implements BeanNameAware {
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
+    }
+
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
+    }
+
     @Override
     public void setBeanName(String beanName) {
         System.out.println(beanName + " bean has been initialized.");
