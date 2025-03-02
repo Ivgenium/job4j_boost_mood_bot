5f567aae9109523f22c0660b6cff1e8232af111c
Ivgenium <genrymorgon@mail.ru>
2025-03-03 01:31:43 +0300

    Механизм рассылки уведомлений [#505297]

---
+++ src/main/java/ru/job4j/bmb/services/RemindService.java
@@ -0,0 +1,45 @@
+package ru.job4j.bmb.services;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.job4j.bmb.repository.UserRepository;
+
+@Service
+public class RemindService implements BeanNameAware {
+    private final TgRemoteService tgRemoteService;
+    private final UserRepository userRepository;
+
+    public RemindService(TgRemoteService tgRemoteService, UserRepository userRepository) {
+        this.tgRemoteService = tgRemoteService;
+        this.userRepository = userRepository;
+    }
+
+    @Scheduled(fixedRateString = "${remind.period}")
+    public void ping() {
+        for (var user : userRepository.findAll()) {
+            var message = new SendMessage();
+            message.setChatId(user.getChatId());
+            message.setText("Ping");
+            tgRemoteService.send(message);
+        }
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println(beanName + " bean has been initialized.");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
+}
